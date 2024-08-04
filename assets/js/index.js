20e0c96fd74c806ace544d574632618672b7c575
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-08-04 13:57:37 +0600

    project title animation done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
 const title = document.querySelector('h1');
 const letters = title.textContent.split('');
-console.log(letters)
+
+title.innerHTML = '';
+
+[...letters].forEach(letter=>{
+    title.innerHTML += `<span class="letter">${letter}</span>`
+});
+
+gsap.set('.letter', {display:'inline-block'});
+gsap.fromTo('.letter', {y:'100%',opacity:0}, {y:0, opacity:1, delay:2, stagger:.05, ease:'back.out(3)'})
